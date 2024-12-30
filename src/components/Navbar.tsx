7118ec5b0a9266ef41623ef0582e9a8cf6185edb
Lucas Gonzalez <luc4s92.g@gmail.com>
2024-12-30 12:10:51 -0300

    Navbar added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,116 @@
+import { useEffect, useRef, useState } from "react";
+import clsx from "clsx";
+import { gsap } from "gsap";
+
+import { useWindowScroll } from "react-use";
+
+const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
+
+const Navbar = () => {
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+  const [isNavVisible, setIsNavVisible] = useState(true);
+
+  const navContainerRef = useRef<HTMLDivElement | null>(null);
+  const audioElementRef = useRef<HTMLAudioElement | null>(null);
+
+  const { y: currentScrollY } = useWindowScroll();
+  const lastScrollY = useRef(0);
+
+  // Efecto para controlar la visibilidad del navbar según el scroll y aplicar la clase floating-nav
+  useEffect(() => {
+    if (navContainerRef.current) {
+      if (currentScrollY === 0) {
+        setIsNavVisible(true);
+        navContainerRef.current.classList.remove("floating-nav");
+      } else if (currentScrollY > lastScrollY.current) {
+        setIsNavVisible(false);
+        navContainerRef.current.classList.add("floating-nav");
+      } else if (currentScrollY < lastScrollY.current) {
+        setIsNavVisible(true);
+        navContainerRef.current.classList.add("floating-nav");
+      }
+    }
+    lastScrollY.current = currentScrollY; // Actualizamos el último scrollY
+  }, [currentScrollY]);
+
+  // Efecto para animar el navbar con GSAP
+  useEffect(() => {
+    if (navContainerRef.current) {
+      gsap.to(navContainerRef.current, {
+        y: isNavVisible ? 0 : -100,
+        opacity: isNavVisible ? 1 : 0,
+        duration: 0.3,
+        ease: "power2.out",
+      });
+    }
+  }, [isNavVisible]);
+
+  // Control de audio
+  const toggleAudioIndicator = () => {
+    setIsAudioPlaying((prev) => !prev);
+    setIsIndicatorActive((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (audioElementRef.current) {
+      if (isAudioPlaying) {
+        audioElementRef.current.play();
+      } else {
+        audioElementRef.current.pause();
+      }
+    }
+  }, [isAudioPlaying]);
+
+  return (
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 top-4 z-50 h-20 border-none transition-all duration-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            <img src="/img/logo.png" alt="logo" className="w-16" />
+          </div>
+          <div className="flex h-full items-center">
+            <div className="hidden md:block">
+              {navItems.map((item, index) => (
+                <a
+                  key={index}
+                  href={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+            <button
+              className="ml-10 flex items-center space-x-0.5"
+              onClick={toggleAudioIndicator}
+            >
+              <audio
+                ref={audioElementRef}
+                className="hidden"
+                src="/audio/dbz.mp3"
+                loop
+              />
+              {[1, 2, 3, 4].map((bar) => (
+                <div
+                  key={bar}
+                  className={clsx("indicator-line", {
+                    active: isIndicatorActive,
+                  })}
+                  style={{
+                    animationDelay: `${bar * 0.1}s`,
+                  }}
+                />
+              ))}
+            </button>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
